8280950c467ca7285a2372ead90b0ca016256aea
tianshi <1147893217@qq.com>
2019-12-12 17:22:32 +0800

    默认的

--- scripts/weblogic_weak_pass.py
+++ scripts/weblogic_weak_pass.py
@@ -17,6 +17,7 @@ import requests
 
 
 def poc(url):
+	
     headers = {
         "User-Agent":"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
         "Content-Type":"application/x-www-form-urlencoded"
